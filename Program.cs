0b330edbeb7a7155fba1c8da364678a8ba9afdea
Anastasia <Atlanika373@yandex.ru>
2022-09-11 00:47:44 +0500

    fill and print new array

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array = { "one", "two", "three", "four", "five", "six", "seven" };
+﻿string[] array = { "one", "two", "three", "four", "five", "six", "seven", "iii", "600", "56", "1"};
 Console.WriteLine("[" + string.Join(", ", array) + "]");
 
 int count = 0;
@@ -8,4 +8,18 @@ for (int i = 0; i < array.Length; i++)
         count++;
 }
 
+Console.WriteLine(count);
+
 string[] finalArray = new string[count];
+
+int k = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        finalArray[k] = array[i];
+        k++;
+    }
+}
+
+Console.WriteLine("[" + string.Join(", ", finalArray) + "]");
