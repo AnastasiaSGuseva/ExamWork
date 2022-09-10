b7e9ca1f9ce20706f335eed5b7ba454702b4a8ae
Anastasia <Atlanika373@yandex.ru>
2022-09-11 00:38:49 +0500

    add new array

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string [] array = {"one", "two", "three", "four", "five", "six", "seven"};
+﻿string[] array = { "one", "two", "three", "four", "five", "six", "seven" };
 Console.WriteLine("[" + string.Join(", ", array) + "]");
 
 int count = 0;
@@ -7,3 +7,5 @@ for (int i = 0; i < array.Length; i++)
     if (array[i].Length <= 3)
         count++;
 }
+
+string[] finalArray = new string[count];
