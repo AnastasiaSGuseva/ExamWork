7667c6d2ac435644a27ef1169683a2b0a23d33f3
Anastasia <Atlanika373@yandex.ru>
2022-09-11 00:35:53 +0500

    add counter for strings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿string [] array = {"one", "two", "three", "four", "five", "six", "seven"};
 Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+        count++;
+}
